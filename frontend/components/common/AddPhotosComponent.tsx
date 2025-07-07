fc322b6684185c408a740f235cf475c09f30d0f3
Kartik Tiwari <tiwari2000kartik@gmail.com>
2025-07-07 21:55:48 +0900

    Add Photos from Gallery feature added

--- frontend/components/common/AddPhotosComponent.tsx
+++ frontend/components/common/AddPhotosComponent.tsx
@@ -1,21 +1,56 @@
-import { StyleSheet, TouchableOpacity, View, Text,FlatList, Animated } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text,FlatList, Animated, Dimensions, Alert} from 'react-native';
 import React, { useEffect, useState } from 'react';
+import PrimaryButton from '../atoms/PrimaryButton';
+import Modal from 'react-native-modal';
+import * as ImagePicker from 'expo-image-picker';
+
 // import { IconSymbol } from "@/components/ui/IconSymbol";
+
+const { height: SCREEN_HEIGHT } = Dimensions.get('window')
 
 type Props = {};
 
 
-const AddPhotosComponents: React.FC<Props> = (props) => {
+const AddPhotosComponents: React.FC<Props> = async (props) => {
 
     const [photosData, setPhotosData] = useState<number[]>([]);
     const[id,setId] = useState(0)
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [isShowModal, setIsShowModal] = useState(false);
+    const [image, setImage] = useState<string | null>(null);
+
+    // const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    // if (status !== 'granted') {
+    //   Alert.alert('Permission to access media library is required!');
+    //   // return;
+    // }
    
-    const addPhotoHandler=()=>{
-      setId(id=>id+1);
-      const newData = {id: id  };
-      setData( [...data, newData] );
+    // const addPhotoHandler=()=>{
+    //   setId(id=>id+1);
+    //   const newData = {id: id  };
+    //   setData( [...data, newData] );
+    // }
+
+    const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true, // Enables cropping
+      aspect: [4, 3],      // Aspect ratio for cropping
+      quality: 1,          // Highest quality
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    } else {
+      Alert.alert('No image selected');
     }
+  };
+
+    const addPhotoHandler=()=>{
+      setIsShowModal(true);
+    }
+
     const deletePhotoHandler=(id:number)=>{
        setData(prevData => prevData.filter(item => item.id !== id));
 
@@ -36,6 +71,34 @@ const AddPhotosComponents: React.FC<Props> = (props) => {
          </View>
         ) 
         }
+
+        <Modal
+          isVisible={isShowModal}
+          animationIn="slideInUp"
+          animationOut="slideOutDown"
+          onBackdropPress={() => setIsShowModal(false)}
+          backdropOpacity={0}
+          style={{ justifyContent: 'flex-end', margin: 20, }}
+        >
+          <View style={{
+            height: SCREEN_HEIGHT * 0.2,
+            backgroundColor: 'lightblue',
+            borderTopLeftRadius: 18,
+            borderTopRightRadius: 18,
+            alignItems: 'center',
+            marginHorizontal: 5,
+            justifyContent: 'center',
+            zIndex: 2,
+          }}>
+              <View style={{width:250}}>
+                <PrimaryButton isActive={true} submitHandler={()=>{}} title="Click a new Photo" />
+                <PrimaryButton isActive={true} submitHandler={pickImage} title="Import from Gallery" />
+                <PrimaryButton isActive={true} submitHandler={()=>setIsShowModal(false)} title="Cancel" />
+              </View>
+          </View>
+          
+          
+        </Modal>
     </View>
   );
 };
