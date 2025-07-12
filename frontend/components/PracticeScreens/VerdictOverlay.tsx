69cc383a81a9ec5470310a5e1d6b336f5830ef8f
ssury2207 <me.shubsury@gmail.com>
2025-07-13 00:13:40 +0900

    added-overlay

---
+++ frontend/components/PracticeScreens/VerdictOverlay.tsx
@@ -0,0 +1,44 @@
+// OverlayScreen.tsx
+import { View, Text, Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Title from '../atoms/Title';
+import NormalText from '../atoms/NormalText';
+import PrimaryButton from '../atoms/PrimaryButton';
+
+export default function VerdictOverlay() {
+  const navigation = useNavigation();
+
+  const overlayButtonHandler = () => {
+    navigation.goBack();
+  };
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.6)',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'white',
+          padding: 20,
+          borderRadius: 12,
+          width: '80%',
+        }}
+      >
+        <Title title="Congratulations!!" />
+        <NormalText text="Your answer is correct" />
+        <NormalText text="You have earned +2 points" />
+        <NormalText text="Your Streak is updated " />
+
+        <PrimaryButton
+          isActive={true}
+          submitHandler={overlayButtonHandler}
+          title="Review Detailed Answer"
+        />
+      </View>
+    </View>
+  );
+}
