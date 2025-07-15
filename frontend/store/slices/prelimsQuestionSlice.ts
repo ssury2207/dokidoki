f374bcd728ef9eac933db3289f7d35c759c81f41
ssury2207 <me.shubsury@gmail.com>
2025-07-15 23:19:07 +0900

    setup-slice

---
+++ frontend/store/slices/prelimsQuestionSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface prelimsQuestionState {
+  question: string;
+  options: string[];
+  actualOption: string;
+  expectedOption: string;
+}
+
+const initialState: prelimsQuestionState = {
+  question: '',
+  options: [],
+  actualOption: '',
+  expectedOption: '',
+};
+
+const prelimsQuestionSlice = createSlice({
+  name: 'prelimsQuestionSlice',
+  initialState,
+  reducers: {
+    setQuestion(state, action: PayloadAction<string>) {
+      state.question = action.payload;
+    },
+    setOptions(state, action: PayloadAction<string[]>) {
+      state.options = action.payload;
+    },
+    setActualOption(state, action: PayloadAction<string>) {
+      state.actualOption = action.payload;
+    },
+    setExpectedOption(state, action: PayloadAction<string>) {
+      state.expectedOption = action.payload;
+    },
+  },
+});
+
+export const { setQuestion, setOptions, setActualOption, setExpectedOption } =
+  prelimsQuestionSlice.actions;
+export default prelimsQuestionSlice.reducer;
